10f6e891f0f331de18137e7d586828f4bc35ecc4
Fredrik Eilertsen <fredrik.eilertsen@iwmac.no>
2018-12-25 19:58:47 +0100

    Make detail cells not interactive except those that should be

--- Firmadetaljer/FirmDetailsTableViewController.swift
+++ Firmadetaljer/FirmDetailsTableViewController.swift
@@ -30,14 +30,20 @@ class FirmDetailsTableViewController: UITableViewController {
         return cell
     }
     
+    private func buildNonInteractiveCell(_ name:String, description:String) -> UITableViewCell {
+        let cell = buildCell(name, description: description)
+        cell.isUserInteractionEnabled = false
+        return cell
+    }
+    
     private func buildNaeringskodeCells(_ companyNaeringskode:Naeringskode) -> [UITableViewCell] {
         var naeringskodeCellsArray = Array<UITableViewCell>()
         
         if let kode = companyNaeringskode.kode {
-            naeringskodeCellsArray.append(buildCell(NSLocalizedString("Naeringskode-Code", comment: ""), description: kode))
+            naeringskodeCellsArray.append(buildNonInteractiveCell(NSLocalizedString("Naeringskode-Code", comment: ""), description: kode))
         }
         if let beskrivelse = companyNaeringskode.beskrivelse {
-            naeringskodeCellsArray.append(buildCell(NSLocalizedString("Naeringskode-Description", comment: ""), description: beskrivelse))
+            naeringskodeCellsArray.append(buildNonInteractiveCell(NSLocalizedString("Naeringskode-Description", comment: ""), description: beskrivelse))
         }
         
         return naeringskodeCellsArray
@@ -47,25 +53,25 @@ class FirmDetailsTableViewController: UITableViewController {
         var addressCellsArray = Array<UITableViewCell>()
         
         if let adresse = companyAddress.adresse {
-            addressCellsArray.append(buildCell(NSLocalizedString("Adresse-Address", comment: ""), description: adresse))
+            addressCellsArray.append(buildNonInteractiveCell(NSLocalizedString("Adresse-Address", comment: ""), description: adresse))
         }
         if let postnummer = companyAddress.postnummer {
-            addressCellsArray.append(buildCell(NSLocalizedString("Adresse-ZipCode", comment: ""), description: String(postnummer)))
+            addressCellsArray.append(buildNonInteractiveCell(NSLocalizedString("Adresse-ZipCode", comment: ""), description: String(postnummer)))
         }
         if let poststed = companyAddress.poststed {
-            addressCellsArray.append(buildCell(NSLocalizedString("Adresse-City", comment: ""), description: poststed))
+            addressCellsArray.append(buildNonInteractiveCell(NSLocalizedString("Adresse-City", comment: ""), description: poststed))
         }
         if let kommunenummer = companyAddress.kommunenummer {
-            addressCellsArray.append(buildCell(NSLocalizedString("Adresse-MunicipalityNumber", comment: ""), description: String(kommunenummer)))
+            addressCellsArray.append(buildNonInteractiveCell(NSLocalizedString("Adresse-MunicipalityNumber", comment: ""), description: String(kommunenummer)))
         }
         if let kommune = companyAddress.kommune {
-            addressCellsArray.append(buildCell(NSLocalizedString("Adresse-Municipal", comment: ""), description: kommune))
+            addressCellsArray.append(buildNonInteractiveCell(NSLocalizedString("Adresse-Municipal", comment: ""), description: kommune))
         }
         if let landkode = companyAddress.landkode {
-            addressCellsArray.append(buildCell(NSLocalizedString("Adresse-CountryCode", comment: ""), description: landkode))
+            addressCellsArray.append(buildNonInteractiveCell(NSLocalizedString("Adresse-CountryCode", comment: ""), description: landkode))
         }
         if let land = companyAddress.land {
-            addressCellsArray.append(buildCell(NSLocalizedString("Adresse-Country", comment: ""), description: land))
+            addressCellsArray.append(buildNonInteractiveCell(NSLocalizedString("Adresse-Country", comment: ""), description: land))
         }
         
         return addressCellsArray
@@ -80,32 +86,32 @@ class FirmDetailsTableViewController: UITableViewController {
         var mainSection = SectionCellData(header: NSLocalizedString("Detail-Section", comment: ""))
         
         if let navn = company?.navn {
-            mainSection.cells.append(buildCell(NSLocalizedString("Detail-FirmName", comment: ""), description: navn))
+            mainSection.cells.append(buildNonInteractiveCell(NSLocalizedString("Detail-FirmName", comment: ""), description: navn))
             self.title = navn
         }
         
         if let orgnr = company?.organisasjonsnummer {
-            mainSection.cells.append(buildCell(NSLocalizedString("Detail-Org.number", comment: ""), description: String(orgnr)))
+            mainSection.cells.append(buildNonInteractiveCell(NSLocalizedString("Detail-Org.number", comment: ""), description: String(orgnr)))
         }
         
         if let stiftelsesdato = company?.stiftelsesdato {
-            mainSection.cells.append(buildCell(NSLocalizedString("Detail-DateOfEstablishment", comment: ""), description: stiftelsesdato))
+            mainSection.cells.append(buildNonInteractiveCell(NSLocalizedString("Detail-DateOfEstablishment", comment: ""), description: stiftelsesdato))
         }
         
         if let registreringsdatoEnhetsregisteret = company?.registreringsdatoEnhetsregisteret {
-            mainSection.cells.append(buildCell(NSLocalizedString("Detail-Enhetsregisteret", comment: ""), description: decodeYesOrNo(registreringsdatoEnhetsregisteret)))
+            mainSection.cells.append(buildNonInteractiveCell(NSLocalizedString("Detail-Enhetsregisteret", comment: ""), description: decodeYesOrNo(registreringsdatoEnhetsregisteret)))
         }
         
         if let oppstartsdato = company?.oppstartsdato {
-            mainSection.cells.append(buildCell(NSLocalizedString("Detail-StartingDate", comment: ""), description: oppstartsdato))
+            mainSection.cells.append(buildNonInteractiveCell(NSLocalizedString("Detail-StartingDate", comment: ""), description: oppstartsdato))
         }
         
         if let datoEierskifte = company?.datoEierskifte {
-            mainSection.cells.append(buildCell(NSLocalizedString("Detail-ChangeOfOwnershipDate", comment: ""), description: datoEierskifte))
+            mainSection.cells.append(buildNonInteractiveCell(NSLocalizedString("Detail-ChangeOfOwnershipDate", comment: ""), description: datoEierskifte))
         }
         
         if let organisasjonsform = company?.organisasjonsform {
-            mainSection.cells.append(buildCell(NSLocalizedString("Detail-OrganizationalForm", comment: ""), description: organisasjonsform))
+            mainSection.cells.append(buildNonInteractiveCell(NSLocalizedString("Detail-OrganizationalForm", comment: ""), description: organisasjonsform))
         }
         
         if let hjemmeside = company?.hjemmeside {
@@ -115,43 +121,43 @@ class FirmDetailsTableViewController: UITableViewController {
         }
         
         if let registrertIFrivillighetsregisteret = company?.registrertIFrivillighetsregisteret {
-            mainSection.cells.append(buildCell(NSLocalizedString("Detail-Frivillighetsregisteret", comment: ""), description: decodeYesOrNo(registrertIFrivillighetsregisteret)))
+            mainSection.cells.append(buildNonInteractiveCell(NSLocalizedString("Detail-Frivillighetsregisteret", comment: ""), description: decodeYesOrNo(registrertIFrivillighetsregisteret)))
         }
         
         if let registrertIMvaregisteret = company?.registrertIMvaregisteret {
-            mainSection.cells.append(buildCell(NSLocalizedString("Detail-Mvaregisteret", comment: ""), description: decodeYesOrNo(registrertIMvaregisteret)))
+            mainSection.cells.append(buildNonInteractiveCell(NSLocalizedString("Detail-Mvaregisteret", comment: ""), description: decodeYesOrNo(registrertIMvaregisteret)))
         }
         
         if let registrertIForetaksregisteret = company?.registrertIForetaksregisteret {
-            mainSection.cells.append(buildCell(NSLocalizedString("Detail-Foretaksregisteret", comment: ""), description: decodeYesOrNo(registrertIForetaksregisteret)))
+            mainSection.cells.append(buildNonInteractiveCell(NSLocalizedString("Detail-Foretaksregisteret", comment: ""), description: decodeYesOrNo(registrertIForetaksregisteret)))
         }
         
         if let registrertIStiftelsesregisteret = company?.registrertIStiftelsesregisteret {
-            mainSection.cells.append(buildCell(NSLocalizedString("Detail-Stiftelsesregisteret", comment: ""), description: decodeYesOrNo(registrertIStiftelsesregisteret)))
+            mainSection.cells.append(buildNonInteractiveCell(NSLocalizedString("Detail-Stiftelsesregisteret", comment: ""), description: decodeYesOrNo(registrertIStiftelsesregisteret)))
         }
         
         if let frivilligRegistrertIMvaregisteret = company?.frivilligRegistrertIMvaregisteret {
-            mainSection.cells.append(buildCell(NSLocalizedString("Detail-Mvaregisteret-Voluntarily", comment: ""), description: decodeYesOrNo(frivilligRegistrertIMvaregisteret)))
+            mainSection.cells.append(buildNonInteractiveCell(NSLocalizedString("Detail-Mvaregisteret-Voluntarily", comment: ""), description: decodeYesOrNo(frivilligRegistrertIMvaregisteret)))
         }
         
         if let antallAnsatte = company?.antallAnsatte {
-            mainSection.cells.append(buildCell(NSLocalizedString("Detail-Employees", comment: ""), description: String(antallAnsatte)))
+            mainSection.cells.append(buildNonInteractiveCell(NSLocalizedString("Detail-Employees", comment: ""), description: String(antallAnsatte)))
         }
         
         if let sisteInnsendteAarsregnskap = company?.sisteInnsendteAarsregnskap {
-            mainSection.cells.append(buildCell(NSLocalizedString("Detail-LatestSubmittedAnnualAccounts", comment: ""), description: String(sisteInnsendteAarsregnskap)))
+            mainSection.cells.append(buildNonInteractiveCell(NSLocalizedString("Detail-LatestSubmittedAnnualAccounts", comment: ""), description: String(sisteInnsendteAarsregnskap)))
         }
         
         if let konkurs = company?.konkurs {
-            mainSection.cells.append(buildCell(NSLocalizedString("Detail-Bankrupt", comment: ""), description: decodeYesOrNo(konkurs)))
+            mainSection.cells.append(buildNonInteractiveCell(NSLocalizedString("Detail-Bankrupt", comment: ""), description: decodeYesOrNo(konkurs)))
         }
         
         if let underAvvikling = company?.underAvvikling {
-            mainSection.cells.append(buildCell(NSLocalizedString("Detail-UnderLiquidation", comment: ""), description: decodeYesOrNo(underAvvikling)))
+            mainSection.cells.append(buildNonInteractiveCell(NSLocalizedString("Detail-UnderLiquidation", comment: ""), description: decodeYesOrNo(underAvvikling)))
         }
         
         if let underTvangsavviklingEllerTvangsopplosning = company?.underTvangsavviklingEllerTvangsopplosning {
-            mainSection.cells.append(buildCell(NSLocalizedString("Detail-ForcedResolution", comment: ""), description: decodeYesOrNo(underTvangsavviklingEllerTvangsopplosning)))
+            mainSection.cells.append(buildNonInteractiveCell(NSLocalizedString("Detail-ForcedResolution", comment: ""), description: decodeYesOrNo(underTvangsavviklingEllerTvangsopplosning)))
         }
         
         if let overordnetEnhet = company?.overordnetEnhet {
@@ -166,11 +172,11 @@ class FirmDetailsTableViewController: UITableViewController {
             var sektorkodeSection = SectionCellData(header: NSLocalizedString("InstitutionalSectorCode-Section", comment: ""))
             
             if let kode = institusjonellSektorkode.kode {
-                sektorkodeSection.cells.append(buildCell(NSLocalizedString("InstitutionalSectorCode-Code", comment: ""), description: String(kode)))
+                sektorkodeSection.cells.append(buildNonInteractiveCell(NSLocalizedString("InstitutionalSectorCode-Code", comment: ""), description: String(kode)))
             }
             
             if let beskrivelse = institusjonellSektorkode.beskrivelse {
-                sektorkodeSection.cells.append(buildCell(NSLocalizedString("InstitutionalSectorCode-Description", comment: ""), description: beskrivelse))
+                sektorkodeSection.cells.append(buildNonInteractiveCell(NSLocalizedString("InstitutionalSectorCode-Description", comment: ""), description: beskrivelse))
             }
             
             sections.append(sektorkodeSection)
