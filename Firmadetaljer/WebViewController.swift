9ee3abbc0f8a11a23180bb79caa8d37a12721bd1
Fredrik Eilertsen <fredrik.eilertsen@iwmac.no>
2019-08-29 01:42:03 +0200

    Replace UIWebView with WKWebView

--- Firmadetaljer/WebViewController.swift
+++ Firmadetaljer/WebViewController.swift
@@ -7,33 +7,44 @@
 //
 
 import UIKit
+import WebKit
 
-class WebViewController: UIViewController, UIWebViewDelegate {
+class WebViewController: UIViewController, WKNavigationDelegate {
 
-    @IBOutlet weak var webView: UIWebView!
-    var urlString: String!
+    @IBOutlet private weak var webView: WKWebView!
     private var activityIndicator: UIActivityIndicatorView!
-    
+    var urlString: String!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         activityIndicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.whiteLarge)
         activityIndicator.center = view.center
         activityIndicator.color = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
         view.addSubview(activityIndicator)
         self.activityIndicator.startAnimating()
-        webView.delegate = self
+        
+        webView.navigationDelegate = self
         let url = URL(string: urlString)!
         let urlRequest = URLRequest(url: url)
-        webView.loadRequest(urlRequest)
+        webView.load(urlRequest)
     }
-    
-    func webViewDidFinishLoad(_ webView: UIWebView) {
+ 
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         self.activityIndicator.stopAnimating()
     }
     
-    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        handleError(error)
+    }
+    
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        handleError(error)
+    }
+    
+    private func handleError(_ error : Error) {
         self.activityIndicator.stopAnimating()
-        let alert = UIAlertController(title: NSLocalizedString("ErrorLoadingDataTitle", comment: ""), message: NSLocalizedString("ErrorLoadingDataMessage", comment: ""), preferredStyle: .alert)
+        let alert = UIAlertController(title: NSLocalizedString("ErrorLoadingDataTitle", comment: ""), message: error.localizedDescription, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
